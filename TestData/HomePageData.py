dff2e55ee6073f73683eccb7cd01c06055e082dc
rohit <rohitnagar1993@gmail.com>
2020-04-25 18:06:14 +0530

    my second commit

--- TestData/HomePageData.py
+++ TestData/HomePageData.py
@@ -3,7 +3,7 @@ import openpyxl
 
 class HomePageData:
 
-    test_HomePage_data = [{"firstname":"Rahul","lastname":"shetty","gender":"Male"}, {"firstname":"Anshika", "lastname":"shetty", "gender":"Female"}]
+    test_HomePage_data = [{"firstname":"Rohit","lastname":"Nagar","gender":"Male"}, {"firstname":"laddo", "lastname":"Bhati", "gender":"Female"}]
 
     @staticmethod
     def getTestData(test_case_name):
